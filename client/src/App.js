eb393eea9b2cbbfadb961a3e529896590958cced
Osa Isacson <asaisacson@gmail.com>
2019-10-15 16:06:54 +0200

    add tabs

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,9 @@ import Active from "./components/Active";
 import Discussed from "./components/Discussed";
 import Suggested from "./components/Suggested";
 
+import Tabs from "react-bootstrap/Tabs";
+import Tab from "react-bootstrap/Tab";
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -23,55 +26,61 @@ class App extends React.Component {
         <header className="section">
           <img src={"/xr-logo.png"} className="logo" alt="logo" />
         </header>
-        {/* <Notifications /> */}
 
-        <div className="section">
-          <br></br>
-          <h1>DEMANDS</h1>
-          <br></br>
-          <h2>How it works</h2>
-          <p>
-            Since nobody has time for long and tedious discussions, the
-            collaborative demand system instead works like this (a merry mix of
-            reddit, github and wikipedia):
-          </p>
-          <br></br>
-          <p>
-            - Anyone can suggest a demand, these end up in the bottom
-            'suggested' section
-          </p>
-          <p>
-            - If 1000 people agree with your demand, it moves to the 'Discussed'
-            section
-          </p>
-          <p>
-            - Once in 'Discussed' the options for adding suggested improvements
-            to your demand are open
-          </p>
-          <p>
-            - If 20 people agree with a suggested change then that change is
-            accepted to the demand description
-          </p>
-          <p>
-            - Once the demand definition is completed and has 10,000 upvotes it
-            gets sent as a petition to the representative who can make the next
-            move, and the demand moves to the 'active' section.
-          </p>
-          <p>
-            - In 'Active' we can see where the demand is at, and what you can do
-            to push it forward.
-          </p>
-          <p>
-            - If a demand gets stuck, you'll find here how and where you can
-            rebel.
-          </p>
-        </div>
-
-        <Active />
-
-        {/* <Discussed />
-
-        <Suggested /> */}
+        <Tabs defaultActiveKey="home" id="uncontrolled-tab-example">
+          <Tab eventKey="home" title="XR">
+            <div className="section">
+              <br></br>
+              <h1>DEMANDS</h1>
+              <br></br>
+              <h2>How it works</h2>
+              <p>
+                Since nobody has time for long and tedious discussions, the
+                collaborative demand system instead works like this (a merry mix
+                of reddit, github and wikipedia):
+              </p>
+              <br></br>
+              <p>
+                - Anyone can suggest a demand, these end up in the bottom
+                'suggested' section
+              </p>
+              <p>
+                - If 1000 people agree with your demand, it moves to the
+                'Discussed' section
+              </p>
+              <p>
+                - Once in 'Discussed' the options for adding suggested
+                improvements to your demand are open
+              </p>
+              <p>
+                - If 20 people agree with a suggested change then that change is
+                accepted to the demand description
+              </p>
+              <p>
+                - Once the demand definition is completed and has 10,000 upvotes
+                it gets sent as a petition to the representative who can make
+                the next move, and the demand moves to the 'active' section.
+              </p>
+              <p>
+                - In 'Active' we can see where the demand is at, and what you
+                can do to push it forward.
+              </p>
+              <p>
+                - If a demand gets stuck, you'll find here how and where you can
+                rebel.
+              </p>
+            </div>
+          </Tab>
+          <Tab eventKey="active" title="Active">
+            <Active />
+          </Tab>
+          <Tab eventKey="discussed" title="Discussed">
+            <Discussed />
+          </Tab>
+          <Tab eventKey="suggested" title="Suggested">
+            <Suggested />
+          </Tab>
+        </Tabs>
       </div>
     );
   }
