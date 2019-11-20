67e311fe0cdd3585a558498c5df846507cf8fd65
Osa Isacson <asaisacson@gmail.com>
2019-11-20 20:49:55 +0100

    remove default react items

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import 'font-awesome/css/font-awesome.min.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "font-awesome/css/font-awesome.min.css";
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
