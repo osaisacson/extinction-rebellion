d1c6ca6c7557a96046dfd379e8d2a123d29aa64a
Osa Isacson <asaisacson@gmail.com>
2019-12-04 23:58:53 +0100

    build

---
+++ client/precache-manifest.ecbe2b8393ef65b78785c9db22a22c3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84af0cc671559f9c33aa7dd37baec0b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12eb1ee1719055c2fd87",
+    "url": "/static/css/main.1e6a7410.chunk.css"
+  },
+  {
+    "revision": "c15e6e5fd0d074f258e5",
+    "url": "/static/js/2.ee2e21e0.chunk.js"
+  },
+  {
+    "revision": "12eb1ee1719055c2fd87",
+    "url": "/static/js/main.e018935b.chunk.js"
+  },
+  {
+    "revision": "eb30fbab5b09ead49cb8",
+    "url": "/static/js/runtime~main.44acd2bb.js"
+  }
+]);
--- client/service-worker.js
+++ client/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5e27ca3a25aa731b81f63475d0dc98ec.js"
+  "/precache-manifest.ecbe2b8393ef65b78785c9db22a22c3b.js"
 );
 
 self.addEventListener('message', (event) => {
